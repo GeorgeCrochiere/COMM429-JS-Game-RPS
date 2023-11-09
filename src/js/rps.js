837a6af37057e95afff0466cc0b20ed597d3792c
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-11-09 10:06:22 -0500

    Cleaned up button selection and animation, and added a scoreboard
    In order to make each game faster, the waiting component was removed, and the specific colors that are used to indicate a selected button has been modified. In preparation for finalizing the scores, a scoreboard was implemented.

--- src/js/rps.js
+++ src/js/rps.js
@@ -1,32 +1,53 @@
 function selectUserOption(move) {
-    let randomAiMove = Math.floor(Math.random * 3 + 1);
+    let randomAiMove = Math.floor(Math.random() * 3 + 1);
     // Results: [-2, -1, 0, 1] = [Error, AI Win, Tie, User Win]
     let turnResult = rpsLogic(move, randomAiMove);
 
-    toggleUserInput();
-    stepAI(0, randomAiMove);
-
+    toggleUserInput(true);
+    setMoves(randomAiMove, move);
+    setTimeout(() => {
+        setMoves(randomAiMove, move);
+        toggleUserInput(false);
+    }, 1500);
 }
 
-function stepAI(aiOption, move) {
-    let aiButtons = document.getElementsByClassName('aiOption');
-    let aiWait = document.getElementById('aiWaiting');
-
-    for (let i = 0; i < aiButtons.length; i++) {
-        aiButtons[i].classList.toggle('hidden');
-    }
-    aiWait.classList.toggle('hidden');
-
-    if (aiOption === 1) {
-        let aiMove = document.getElementById('ai' + move.toString());
-        aiMove.disabled.toggle;
-    }
-}
-
-function toggleUserInput() {
+function toggleUserInput(status) {
     let userButtons = document.getElementsByClassName('userOption');
     for (let i = 0; i < userButtons.length; i++) {
-        userButtons[i].disabled.toggle;
+        userButtons[i].disabled = status;
+    }
+}
+
+function setMoves(aiMove, userMove) {
+    userElem = document.getElementById('user' + userMove.toString());
+    aiElem = document.getElementById('ai' + aiMove.toString());
+
+    switch (userMove) {
+        case 1:
+            userElem.classList.toggle('selectedButtonRock');
+            break;
+        case 2:
+            userElem.classList.toggle('selectedButtonPaper');
+            break;
+        case 3:
+            userElem.classList.toggle('selectedButtonScissors');
+            break;
+        default:
+            break;
+    }
+
+    switch (aiMove) {
+        case 1:
+            aiElem.classList.toggle('selectedButtonRock');
+            break;
+        case 2:
+            aiElem.classList.toggle('selectedButtonPaper');
+            break;
+        case 3:
+            aiElem.classList.toggle('selectedButtonScissors');
+            break;
+        default:
+            break;
     }
 }
 
